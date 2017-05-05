066257efee9144ca67ff0707e12340d2159317b7
TomasHaver <tghandersen@gmail.com>
2017-05-05 15:08:11 +1200

    Merged the Unity and main repos together.

---
+++ Unity/Assets/HandEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(HandController))]
+public class HandEditor:Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		
+		HandController hand = (HandController)target;
+		if (GUILayout.Button("Connect Serial"))
+		{
+			bool opened = hand.SerialConnect();
+			if (opened) Debug.Log("Serial port opened.");
+		}
+		
+		if (GUILayout.Button("Disconnect Serial"))
+		{
+			bool closed = hand.SerialDisconnect();
+			if (closed) Debug.Log("Serial port closed.");
+		}
+		
+		if (GUILayout.Button("Calibrate Minimum"))
+		{
+			hand.CalibrateMinimum();
+		}
+		
+		if (GUILayout.Button("Calibrate Maximum"))
+		{
+			hand.CalibrateMaximum();
+		}
+	}
+}
